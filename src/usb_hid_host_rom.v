bd5d1917418e911939c5b7ffc01b33f671d5af69
epsilon <ruben.lysens2@gmail.com>
2024-04-27 16:06:03 +0200

    whitespace

--- src/usb_hid_host_rom.v
+++ src/usb_hid_host_rom.v
@@ -1,8 +1,9 @@
-module usb_hid_host_rom(
-	input wire clk,
-	input wire [13:0] adr,
-	output reg [4:0] data);
-	reg [4:0] mem [0:616];
-	initial $readmemh("usb_hid_host_rom.hex", mem);
-	always @(posedge clk) data <= mem[adr[9:0]];
+module usb_hid_host_rom (
+    input wire clk,
+    input wire [13:0] adr,
+    output reg [4:0] data
+);
+  reg [4:0] mem[0:616];
+  initial $readmemh("usb_hid_host_rom.hex", mem);
+  always @(posedge clk) data <= mem[adr[9:0]];
 endmodule
